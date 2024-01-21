de1add006813a1cb255c0adf386a855a07ed4665
Avi <avibanerjee2000@gmail.com>
2024-01-22 03:49:09 +0530

    .

--- GadgetNymph/src/main.jsx
+++ GadgetNymph/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.scss'
-import Router from './router.jsx'
+import Router from './RouterFile.jsx'
 import { useState } from 'react'
 
 
